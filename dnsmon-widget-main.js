5ecbcf4b8dddb7961c1491eeeb315a619b4b6e05
Massimo Candela <mcandela@ripe.net>
2019-01-17 15:09:03 +0100

    DAR-3483: [DNSMON] - update widget injector code

--- dnsmon-widget-main.js
+++ dnsmon-widget-main.js
@@ -21,6 +21,10 @@
 
 DNSMON_WIDGET_URL = ((typeof DNSMON_EXTERNAL_WIDGET_URL == 'undefined') ? "https://www-static.ripe.net/static/rnd-ui/dnsmon/static/visualisation/dnsmon/" : DNSMON_EXTERNAL_WIDGET_URL) ;
 
+
+/**
+ * Name space configuration
+ */
 DNSMON_ENVIRONMENT_URL = DNSMON_WIDGET_URL + "environment/";
 
 DNSMON_LIB_URL = DNSMON_WIDGET_URL + "lib/";
@@ -41,67 +45,209 @@ DNSMON_INSTANCES = [];
 DNSMON_RUNNING_INSTANCES = {};
 DNSMON_INSTANCE_CALLBACKS = {};
 
-document.write('<script src="' + DNSMON_LIB_URL + 'require.min.js"></script>');
+window.atlas = window.atlas || {};
+window.atlas._widgets = window.atlas._widgets || {};
+window.atlas._widgets.dnsmon = window.atlas._widgets.dnsmon || {};
+
+window.atlas._widgets.dnsmon.instances = window.atlas._widgets.dnsmon.instances || {
+        requested: [],
+        running: {},
+        callback: {}
+    };
+
+if (!window.atlas._widgets.widgetInjectorRequested) { // Only one injector
+    window.atlas._widgets.widgetInjectorLoaded = false;
+    window.atlas._widgets.widgetInjectorRequested = true;
+    window.atlas._widgets.dnsmon.tmp_scripts = document.getElementsByTagName('script');
+    window.atlas._widgets.dnsmon.tmp_scrip = window.atlas._widgets.dnsmon.tmp_scripts[window.atlas._widgets.dnsmon.tmp_scripts.length - 1];
+    window.atlas._widgets.injectorScript = document.createElement('script');
+    window.atlas._widgets.injectorScript.async = false;
+    window.atlas._widgets.injectorScript.src = DNSMON_LIB_URL + 'require.min.js';
+    window.atlas._widgets.dnsmon.tmp_scrip.parentNode.appendChild(window.atlas._widgets.injectorScript);
+}
 
 
 /**
  * This is the code of the widget system
  */
 
-function getDNSmonInstance(){
-    return DNSMON_INSTANCES.shift();
-}
+// function getDNSmonInstance(){
+//     return DNSMON_INSTANCES.shift();
+// }
 
-function setDNSmonParam(domElement, params){
-    var instance = DNSMON_RUNNING_INSTANCES[domElement];
-    instance.setParams(params);
-}
+// function setDNSmonParam(domElement, params){
+//     var instance = DNSMON_RUNNING_INSTANCES[domElement];
+//     instance.setParams(params);
+// }
 
+// function initDNSmon(domElement, instanceParams, queryParams){
+//     var runDNSmon;
+//
+//     DNSMON_INSTANCES.push({domElement: domElement, instanceParams: instanceParams, queryParams: queryParams, callbacks: {}});
+//
+//     runDNSmon = function (DNSmon) {
+//         var instance;
+//
+//         instance = getDNSmonInstance();
+//         DNSMON_RUNNING_INSTANCES[domElement] = DNSmon(instance);
+//     };
+//
+//
+//     if (!instanceParams.dev) {
+//
+//         require([DNSMON_WIDGET_URL + 'dnsmon-dist.js'], function () {
+//             require(['dnsmon/dnsmon-loader'], runDNSmon);
+//         });
+//
+//     } else {
+//
+//         require([DNSMON_WIDGET_URL + 'dnsmon-loader.js'], runDNSmon);
+//
+//     }
+//
+//
+//     return {
+//         setParams: function(params){
+//             DNSMON_RUNNING_INSTANCES[domElement].setParams(params);
+//         },
+//
+//         on: function(type, callback){
+//             DNSMON_RUNNING_INSTANCES[domElement].on(type, callback);
+//         },
+//
+//         off: function(type){
+//             DNSMON_RUNNING_INSTANCES[domElement].off(type);
+//         },
+//
+//         getParams: function(){
+//             return DNSMON_RUNNING_INSTANCES[domElement].getParams();
+//         },
+//
+//         getEnvironment: function(){
+//             return DNSMON_RUNNING_INSTANCES[domElement].getEnvironment();
+//         }
+//     };
+// }
+
+
+
+/**
+ * Widget injector
+ */
 function initDNSmon(domElement, instanceParams, queryParams){
-    var runDNSmon;
+    var run;
 
-    DNSMON_INSTANCES.push({domElement: domElement, instanceParams: instanceParams, queryParams: queryParams, callbacks: {}});
+    run = function(){
+        var instances, instance;
 
-    runDNSmon = function (DNSmon) {
-        var instance;
+        instances = window.atlas._widgets.dnsmon.instances;
+        instance = instances.requested.shift();
 
-        instance = getDNSmonInstance();
-        DNSMON_RUNNING_INSTANCES[domElement] = DNSmon(instance);
+        while (instance){
+
+            (function(instances, instance){
+                requirejs.config({
+                    waitSeconds: 60
+                });
+                if (instance.instanceParams.dev) { // Load dev version
+                    require([DNSMON_WIDGET_URL + 'dnsmon-loader.js'], function(DNSMON){
+                        instances.running[instance.domElement] = DNSMON(instance);
+                    });
+                } else { // Load deployed version
+                    require([DNSMON_WIDGET_URL + 'dnsmon-dist.js'], function () {
+                        require(['dnsmon/dnsmon-loader'], function(DNSMON){
+                            instances.running[instance.domElement] = DNSMON(instance);
+                        });
+                    });
+                }
+            })(instances, instance);
+
+            instance = instances.requested.shift();
+        }
     };
 
+    window.atlas._widgets.dnsmon.instances.callback[domElement] = null;
+    window.atlas._widgets.dnsmon.instances.requested
+        .push({domElement: domElement, instanceParams: instanceParams, queryParams: queryParams, callbacks: {}});
 
-    if (!instanceParams.dev) {
-
-        require([DNSMON_WIDGET_URL + 'dnsmon-dist.js'], function () {
-            require(['dnsmon/dnsmon-loader'], runDNSmon);
-        });
-
+    if (document.readyState == 'complete'){
+        window.atlas._widgets.widgetInjectorLoaded = true;
     } else {
 
-        require([DNSMON_WIDGET_URL + 'dnsmon-loader.js'], runDNSmon);
+        function ieLoadBugFix(){
+            if (!window.atlas._widgets.widgetInjectorLoaded){
+                if (document.readyState=='loaded' || document.readyState=='complete') {
+                    window.atlas._widgets.injectorScript.onload();
+                }else {
+                    setTimeout(ieLoadBugFix, 200);
+                }
+            }
+        }
 
+        ieLoadBugFix();
     }
 
+    if (window.atlas._widgets.widgetInjectorLoaded === false){
+        window.atlas._widgets.injectorScript.onload = function(){
+            window.atlas._widgets.widgetInjectorLoaded = true;
+            run();
+        };
+    } else {
+        run();
+    }
 
     return {
+        ready: function(callback){
+            window.atlas._widgets.dnsmon.instances.callback[domElement] = callback;
+        },
         setParams: function(params){
-            DNSMON_RUNNING_INSTANCES[domElement].setParams(params);
+            var instance = window.atlas._widgets.dnsmon.instances.running[domElement];
+
+            if (instance) {
+                instance.setParams(params);
+            } else {
+                throw "Widget not loaded yet. Try again in a few seconds."
+            }
         },
 
         on: function(type, callback){
-            DNSMON_RUNNING_INSTANCES[domElement].on(type, callback);
+            var instance = window.atlas._widgets.dnsmon.instances.running[domElement];
+
+            if (instance) {
+                instance.on(type, callback);
+            } else {
+                throw "Widget not loaded yet. Try again in a few seconds."
+            }
         },
 
         off: function(type){
-            DNSMON_RUNNING_INSTANCES[domElement].off(type);
+            var instance = window.atlas._widgets.dnsmon.instances.running[domElement];
+
+            if (instance) {
+                instance.off(type);
+            } else {
+                throw "Widget not loaded yet. Try again in a few seconds."
+            }
         },
 
         getParams: function(){
-            return DNSMON_RUNNING_INSTANCES[domElement].getParams();
+            var instance = window.atlas._widgets.dnsmon.instances.running[domElement];
+
+            if (instance) {
+                instance.getParams();
+            } else {
+                throw "Widget not loaded yet. Try again in a few seconds."
+            }
         },
 
         getEnvironment: function(){
-            return DNSMON_RUNNING_INSTANCES[domElement].getEnvironment();
+            var instance = window.atlas._widgets.dnsmon.instances.running[domElement];
+
+            if (instance) {
+                instance.getEnvironment();
+            } else {
+                throw "Widget not loaded yet. Try again in a few seconds."
+            }
         }
     };
 }
